26799c038b149256978f806728f40b4d85689753
Kaylie Austin <kmaustin@stu.naperville203.org>
2026-05-05 11:48:59 -0500

    hii

---
+++ ColorLabs17/PosterProject.java
@@ -0,0 +1,90 @@
+/**
+ * Poster Project
+ * Kaylie Austin 
+ * May 5th, 2026
+ **/ 
+ 
+//one transformation must be a horizontal or vertical mirroring (potentially of the entire collage)
+//one transformation must be an original manipulation of the image(rotate, sepia, blend, etc . . . )
+//one transformation must be a recursive manipulation of the image
+
+// The collage must be saved as an image file
+// ex. picture.write(FileChooser.getMediaPath(fileName));
+// ex. picture.write(“thisfolder.finalcollege.jpg”);
+
+//the orginal image will be put on the canvas on the top right slot
+
+//Vertical Mirroring (DONE), Negation, Grayscale, Blending, Recursion, Hue shifting
+
+//The image is 2560 x 1440 pixels 
+//my canvas that I want to post my 6 images on is 5140 x 3855 pixels
+
+public class PosterProject
+{
+    public static void main(String[] args)
+    {
+         Picture acanvas = new Picture("images/canvas.jpg"); 
+         Picture asucculent = new Picture("images/succulent.jpg"); 
+         
+         mirrorVertical(asucculent); 
+         
+         asucculent.explore();
+         copytoCanvas(asucculent, acanvas);
+         acanvas.explore();
+    }
+        
+    
+//MIRRORS THE IMAGE 
+    /**
+     * Method to mirror a vertical line in the middle of the picture based on the width 
+     */
+    
+        public static void mirrorVertical(Picture source)
+        {
+            int width = source.getWidth();
+            int mirrorPoint = width/2;
+            Pixel leftPixel = null;
+            Pixel rightPixel = null;
+            
+            //loop through all of the rows
+            for (int y = 0; y < source.getHeight(); y++)
+            {
+                //loop from 0 to the middle (mirror Point)
+                for (int x = 0; x < mirrorPoint; x++)
+                {
+                    leftPixel = source.getPixel(x, y);
+                    rightPixel = source.getPixel(width - 1 - x, y);
+                    rightPixel.setColor(leftPixel.getColor());
+                    
+                }
+            }
+        } //mirrorVertical 
+
+
+        
+        
+        
+//COPY TO CANVAS (FIX??)
+    /**
+     * Copy one picture to another pic/canvas
+     * Add two ints to the paramaters to place you want picture on the target
+     */
+    
+    public static void copytoCanvas(Picture source, Picture target)
+    {
+        Pixel sourcePix = null;
+        Pixel targetPix = null;
+        
+        //loop through jcollums (targetX is the starting point on Canvas)
+        for (int sourceX = 0, targetX = 0; sourceX < source.getWidth(); sourceX++, targetX++)
+        {
+            for (int sourceY = 0, targetY = 0; sourceY < source.getWidth(); sourceY++, targetY++)
+            {
+                sourcePix = source.getPixel(sourceX, sourceY);
+                targetPix = target.getPixel(targetX, targetY);
+                targetPix.setColor(sourcePix.getColor());
+            }
+        }
+    }
+    
+} //class
